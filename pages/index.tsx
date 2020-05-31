433cc9b4742b4d99b80dc634589dc327257ae83b
Juan Martín Seery <me@juanm04.com>
2020-05-31 12:47:37 -0300

    Added create button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
 import Router from "next/router";
 import slugify from "slugify";
-import { Heading, Input, InputGroup, InputRightAddon } from "@chakra-ui/core";
+import {
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightAddon,
+} from "@chakra-ui/core";
 import { Check } from "react-feather";
 
 export default () => {
   const [inputValue, setInputValue] = useState("");
 
-  function handleInput() {
-    let slug = slugify(inputValue, { lower: true, remove: /[^\w ]+/g });
-    let room = slug !== "" ? slug : Math.random().toString(36).substring(7);
+  function handleCreate() {
+    Router.push(
+      "/[room]",
+      `/${Math.random().toString(36).substring(7)}?host=true`
+    );
+  }
 
-    Router.push("/[room]", `/${room}?host=true`);
+  function handleJoin() {
+    let room = slugify(inputValue, { lower: true, remove: /[^\w ]+/g });
+    if (room === "") alert("Inavlid room");
+
+    Router.push("/[room]", `/${room}`);
   }
 
   return (
@@ -19,22 +32,24 @@ export default () => {
       <Heading as="h1" size="2xl">
         WatchWith
       </Heading>
-      <Heading as="h2" size="sm">
+      <Heading as="h2" size="sm" paddingBottom="30px">
         Watch media from Youtube, Twitch, Facebook, direct files and more
         synchronously
       </Heading>
-      <InputGroup size="md" paddingTop="30px">
+      <Button onClick={handleCreate}>Create Room</Button>
+      <InputGroup size="md" paddingTop="10px">
         <Input
-          placeholder="Create room"
+          placeholder="Join Room"
           value={inputValue}
+          isRequired
           onChange={(e) => setInputValue(e.target.value)}
           onKeyPress={(e) => {
-            if (e.key === "Enter") handleInput();
+            if (e.key === "Enter") handleJoin();
           }}
         />
         <InputRightAddon
           children={<Check />}
-          onClick={handleInput}
+          onClick={handleJoin}
           cursor="pointer"
         />
       </InputGroup>
